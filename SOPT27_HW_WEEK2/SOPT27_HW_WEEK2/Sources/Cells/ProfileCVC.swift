75c85c33bd4d4baa49fec781226462c13dade979
qodhrkawk <gabriel1120@naver.com>
2020-11-06 12:22:55 +0900

    3주차 과제 추가(2주차 폴더에 추가로 만듬)

---
+++ SOPT27_HW_WEEK2/SOPT27_HW_WEEK2/Sources/Cells/ProfileCVC.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileCVC.swift
+//  SOPT27_HW_WEEK2
+//
+//  Created by Yunjae Kim on 2020/11/06.
+//
+
+import UIKit
+
+class ProfileCVC: UICollectionViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var yellowLine: UIImageView!
+    
+    
+    static let identifier = "ProfileCVC"
+    
+    
+}
---
+++ SOPT27_HW_WEEK2/SOPT27_HW_WEEK2/Sources/VCs/Week3VC.swift
@@ -0,0 +1,115 @@
+//
+//  Week3VC.swift
+//  SOPT27_HW_WEEK2
+//
+//  Created by Yunjae Kim on 2020/11/05.
+//
+
+import UIKit
+
+class Week3VC: UIViewController {
+    
+    @IBOutlet weak var soptBackground: UIImageView!
+    @IBOutlet weak var soptLogoImage: UIImageView!
+    @IBOutlet weak var profileEditButton: UIButton!
+    @IBOutlet weak var profileCollectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setItems()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func setItems(){
+        
+        soptLogoImage.image = UIImage(named: "soptLogo.png")
+        soptBackground.image = UIImage(named: "banner")
+        profileEditButton.layer.cornerRadius = 5
+        
+        profileCollectionView.dataSource = self
+        profileCollectionView.delegate = self
+        
+        
+        
+    }
+    
+    
+    
+}
+
+
+extension Week3VC : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier:                                                            ProfileCVC.identifier, for: indexPath) as? ProfileCVC else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+        
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "reusableView", for: indexPath)
+            
+            return headerView
+            
+        default:
+            assert(false)
+        }
+    
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+extension Week3VC : UICollectionViewDelegateFlowLayout {
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 197)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 28, left: 24, bottom: 0, right: 24)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 420)
+    }
+    
+    
+}
+
+
+
